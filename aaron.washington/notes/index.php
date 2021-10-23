f7778b7ebb7bf325c820cd318894efcf794ae227
Aaron Washington <aaronjwashington2@gmail.com>
2021-10-23 18:15:09 -0500

    added php

--- aaron.washington/notes/index.php
+++ aaron.washington/notes/index.php
@@ -49,16 +49,112 @@
     ?>
 
     <hr>
-    <div>This is my name</div>
-    <div>
-        <?php
-        $firstname = "Aaron";
-        $lastname = "Washington";
-        $fullname = "<div>$firstname "."$lastname</div>";
 
-        echo $fullname;
-        ?>
-    </div>
+    <div>This is my name</div>
+
+    <?php
+    $firstname = "Aaron";
+    $lastname = "Washington";
+    $fullname = "<div>$firstname "."$lastname</div>";
+
+    echo $fullname;
+    ?>
+
+    <hr>
+
+    <?php
+    // Superglobal
+    // Add to URL ?name=Aaron
+    echo "<a href='?name=Aaron'>Add Name</a>";
+    echo "<div>My name is {$_GET['name']}</div>";
+
+    // Add to URL ?name=Aaron&type=textarea
+    echo "<a href='?name=Aaron&type=textarea'>Make Text Box</a>";
+    echo "<{$_GET['type']}>My name is {$_GET['name']}</{$_GET['type']}>";
+    ?>
+
+    <hr>
+
+    <?php
+    // Array is a small group of values
+    $colors = array("#EA3D3D","green","blue");
+
+    // Array index always starts with 0
+    echo $colors[2];
+
+    echo "
+        <br />$colors[0]
+        <br />$colors[1]
+        <br />$colors[2]
+    ";
+
+    // Count shows number of items in the array
+    echo count($colors);
+    ?>
+
+    <div style="color:<?= $colors[0] ?>">This text is red</div>
+
+    <hr>
+
+    <?php
+    // Associative Array
+    $colorsAssociative = [
+        "red" => "#f00",
+        "green" => "#0f0",
+        "blue" => "#00f"
+    ]; 
+
+    echo $colorsAssociative['green'];
+    ?>
+
+    <hr>
+
+    <?php
+    // Casting is changing object types
+
+    // Change number to string
+    $c = "$a";
+
+    // Change string to a number
+    $d = $c*1;
+
+    $colorsObject = (object)$colorsAssociative;
+
+    echo "<hr>";
+
+    // Array Index Notation
+    echo $colors[0]."<br />";
+    echo $colorsAssociative['red']."<br />";
+    echo $colorsAssociative[$colors[0]]."<br />";
+
+    // Object Property Notation
+    echo $colorsObject->red."<br />";
+    echo $colorsObject->{$colors[0]}."<br />";
+    ?>
+
+    <hr>
+
+    <?php
+    // Printing all object types to page 
+    print_r($colors);
+    echo "<hr>";
+    print_r($colorsAssociative);
+    echo "<hr>";
+    print_r($colorsObject);
+    echo "<hr>";
+    // Include nice default spacing and layout (HTML by default strips all spacing)
+    echo "<pre>",print_r($colorsObject),"</pre>";
+
+    // Make our first Function!
+    function print_p($v) {
+        echo "<pre>",print_r($v),"</pre>";
+    }
+
+    print_p($_GET);
+
+    ?>
+
+
 
 </body>
 </html>
