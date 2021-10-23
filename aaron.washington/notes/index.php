8897132a3b3d3a0c0c1546a8649488d1bc6c8622
Aaron Washington <aaronjwashington2@gmail.com>
2021-10-23 04:11:32 -0500

    php

--- aaron.washington/notes/index.php
+++ aaron.washington/notes/index.php
@@ -7,6 +7,58 @@
     <title>Document</title>
 </head>
 <body>
-    <?php phpinfo(); ?>
+
+    <?php 
+    echo "<div>Hello World</div>";
+    echo "<div>Goodbye World</div>";
+
+    // Variables
+    $a = 5;
+
+    echo $a;
+
+    // String Interpolation
+    echo "<div>I have $a things</div>";
+    echo '<div>I have $a things</div>';
+
+
+    // ** VALUES **
+
+    // Number
+    // Integer
+    $b = 15;
+    // Floating point number (Float)
+    $b = 0.576;
+
+    $b = 10;
+
+    // String (MUST have quote marks)
+    $name = "Darth Maul";
+
+    // Boolean
+    $isOn = true;
+
+
+    // ** Math **
+    // PEMDAS (Parentheses, Exponents, Multiplication, Division, Addition, Subtraction)
+    echo (5 - 4) * 2;
+
+    // Concatenation (combining strings together)
+    echo "<div>b + a" . " = c</div>";
+    echo "<div>$b + $a = ".($a + $b)."</div>";
+    ?>
+
+    <hr>
+    <div>This is my name</div>
+    <div>
+        <?php
+        $firstname = "Aaron";
+        $lastname = "Washington";
+        $fullname = "<div>$firstname "."$lastname</div>";
+
+        echo $fullname;
+        ?>
+    </div>
+
 </body>
 </html>
