6051b3a05a16f33391dfcb65838274d19a52bc04
Aaron Washington <aaronjwashington2@gmail.com>
2021-10-23 22:26:14 -0500

    new php parts

--- aaron.washington/product_list.php
+++ aaron.washington/product_list.php
@@ -5,33 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>AWesome Stuff</title>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-    </style> 
-    <link rel="stylesheet" href="lib/css/gridsystem.css">
-    <link rel="stylesheet" href="lib/css/styleguide.css">
-    <link rel="stylesheet" href="css/storetheme.css">
+    <?php include "parts/meta.php"; ?>
 </head>
 <body>
 
-    <header class="navbar">
-        <div class="container display-flex">
-            <div class="flex-none">
-                <h1>Store</h1>
-            </div>
-            <div class="flex-stretch"></div>
-            <div class="nav nav-flex flex-none">
-                <ul>
-                    <li><a href="#">Link 1</a></li>
-                    <li><a href="#">Link 2</a></li>
-                    <li><a href="#">Link 3</a></li>
-                </ul>
-            </div>
-        </div>
-    </header>
+    <?php include "parts/navbar.php"; ?>
 
     <div class="container">
         <div class="card-light">
