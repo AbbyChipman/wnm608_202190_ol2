85b3ca1876d2f0af8b936713a6b16ceed966a51f
Aaron Washington <aaronjwashington2@gmail.com>
2021-10-23 22:18:31 -0500

    new php

--- aaron.washington/README.md
+++ aaron.washington/README.md
@@ -6,3 +6,5 @@
 - https://aaronjosephmusic.com/aau/wnm608_202190_ol2/aaron.washington/styleguide/index.html
 
 - https://aaronjosephmusic.com/aau/wnm608_202190_ol2/aaron.washington/notes/index.php
+
+- https://aaronjosephmusic.com/aau/wnm608_202190_ol2/aaron.washington/product_list.php
--- aaron.washington/product_item.php
+++ aaron.washington/product_item.php
@@ -35,13 +35,8 @@
 
     <div class="container">
         <div class="card-light">
-            <h2>Product List</h2>
-            <ul>
-                <li><a href="product_item.php">Product 1</a></li>
-                <li><a href="product_item.php">Product 2</a></li>
-                <li><a href="product_item.php">Product 3</a></li>
-                <li><a href="product_item.php">Product 4</a></li>
-            </ul>
+            <h2>Product Item</h2>
+            <p>This is item something</p>
         </div>
     </div>
     
