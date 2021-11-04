b92aaf605ed028f6d7560e491fb8df2c1a95477d
Aaron Washington <aaronjwashington2@gmail.com>
2021-11-03 23:35:12 -0500

    user form

--- aaron.washington/admin/users.php
+++ aaron.washington/admin/users.php
@@ -19,6 +19,7 @@ if (isset($_GET['action'])) {
             $users[$_GET['id']]->classes = explode(", ", $_POST['user-classes']);
 
             file_put_contents($filename, json_encode($users));
+            header("location:{$_SERVER['PHP_SELF']}?id={$_GET['id']}");
             break;
         case "create":
             break;
