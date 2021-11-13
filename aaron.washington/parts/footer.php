bd36c96348887cb4747692aeb240921f6af561ae
Aaron Washington <aaronjwashington2@gmail.com>
2021-11-13 02:44:57 -0600

    footer

--- aaron.washington/parts/footer.php
+++ aaron.washington/parts/footer.php
@@ -1,8 +1,8 @@
 <footer class="display-flex flex-align-center">
     <nav class="flex-stretch nav">
         <ul class="display-flex flex-align-center">
-            <li class="flex-stretch flex-basis-none"><a href="#">What&apos;s New</a></li>
-            <li class="flex-stretch flex-basis-none"><a href="shop.php?id=Mens">Shop</a></li>
+            <li class="flex-stretch flex-basis-none"><a href="shop.php?category=Everything">What&apos;s New</a></li>
+            <li class="flex-stretch flex-basis-none"><a href="shop.php?category=Everything">Shop</a></li>
             <li class="flex-stretch flex-basis-none"><a href="#">About</a></li>
             <li class="flex-stretch flex-basis-none"><a href="cart.php">Cart</a></li>
         </ul>
