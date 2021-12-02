7ef6df53688e55e076f2c9725d3be0e45ff419bd
Aaron Washington <aaronjwashington2@gmail.com>
2021-12-01 23:22:15 -0600

    admin index

---
+++ aaron.washington/admin/index.php
@@ -0,0 +1,38 @@
+<?php 
+
+include "../lib/php/functions.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Product Admin Page</title>
+    <?php include "../parts/meta.php"; ?>
+</head>
+<body>
+
+    <header class="navbar">
+        <div class="container display-flex" style="margin-bottom: 0;">
+            <div class="flex-none">
+                <h5>Product Admin</h5>
+            </div>
+            <div class="flex-stretch"></div>
+            <nav class="nav nav-flex flex-none">
+                <ul>
+                    <li><a href="<?= $_SERVER['PHP_SELF'] ?>">Product List</a></li>
+                    <li><a href="<?= $_SERVER['PHP_SELF'] ?>?id=new">Add New Product</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+
+    <div class="container" style="padding-top: 3rem;">
+
+    </div>
+
+</body>
